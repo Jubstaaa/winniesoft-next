39ceaf8a01769024265f9761122611db357740b3
İlker <ilkerbalcilartr@gmail.com>
2022-10-26 09:48:56 +0300

    fix ' issue

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -88,8 +88,8 @@ function privacyPolicy() {
         <p>
           Cookies are files with a small amount of data that are commonly used
           as anonymous unique identifiers. These are sent to your browser from
-          the websites that you visit and are stored on your device's internal
-          memory.
+          the websites that you visit and are stored on your device&apos;s
+          internal memory.
         </p>{" "}
         <p>
           This Service does not use these “cookies” explicitly. However, the app
